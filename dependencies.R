3720599740b704f6cc5f82f93ed984ba7dbb3a0f
Mark Brisbane <mark@improvesilence.com>
2021-07-25 15:21:19 +0800

    Fixing Generate-Maps workflow
    Previously tried to use here::here to install dependencies, which obviously needed to be installed itself. Now using shell RScript directly.
    
    (without the shell arg in the workflow .yaml)

--- dependencies.R
+++ dependencies.R
@@ -1,4 +1,4 @@
 if (!require("pacman")) install.packages("pacman")
 library(pacman)
 options("rgdal_show_exportToProj4_warnings"="none")
-pacman::p_load(leaflet, tidyverse, here, rgdal, rgeos, geosphere, sf, googlesheets4, glue)
+pacman::p_load(leaflet, tidyverse, here, rgdal, rgeos, geosphere, sf, googlesheets4, glue, snakecase)
